488496c3ba74d8428b2c1fa0d8f1ad7fb9dbcfc8
ThamerAsaad <109876113+ThamerAsaad@users.noreply.github.com>
2022-11-12 16:58:41 +0300

    done

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+function BMI(weight, height) {
+  let ruselt = weight / (height * height);
+  return ruselt;
+}
+console.log(85, 1.8);
 
+function Status(BodyMass) {
+  if (BodyMass < 18.5) {
+    return "لديك نقص في الوزن";
+  } else if (BodyMass >= 18.5 && BodyMass < 25) {
+    return "وزنك صحي";
+  } else {
+    return "لديك زيادة في الوزن";
+  }
+}
+function calculate() {
+  let weight = document.getElementById("weight").value;
+  let height = document.getElementById("height").value;
+  let bmi_value = BMI(weight, height);
+  let desc = Status(bmi_value);
+  let box = document.getElementById("result");
+
+  box.innerText = bmi_value + " == " + desc;
+}
